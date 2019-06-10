6756ff6e36588b78fe5c2a784ab948de701d21b7
Qsouler <lll123456ly@163.com>
2019-06-10 21:42:28 +0800

    update html program and starting admin manager back

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 # coding:utf8
 from flask import Flask, render_template
+import pymysql
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@127.0.0.1:8889/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:8889/movie"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SECRET_KEY"] = "fafdagag34346h43weaFSDFg45hSFG45"
+# 实例化
 app.debug = True
+db = SQLAlchemy(app)
+
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
 
